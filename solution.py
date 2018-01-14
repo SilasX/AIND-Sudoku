5223072ae63c2d3f7e9819b551441d674c048ea9
Silas Barta <sbarta@gmail.com>
2018-01-13 16:05:33 -0800

    Add functions written from classroom exercises.

--- solution.py
+++ solution.py
@@ -68,8 +68,13 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    # Get all the solved boxes
+    known_vals = [x for x in values if len(values[x]) == 1]
+    # Remove those as a possibility from their peers
+    for known_val in known_vals:
+        for peer in peers[known_val]:
+            values[peer] = values[peer].replace(values[known_val], "")
+    return values
 
 
 def only_choice(values):
@@ -92,8 +97,27 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        # For each option list in the unit, use `matches` to point each option
+        # to the box that still has it as an option. If the element was
+        # already in the list, this is a dupe and ignore it instead
+        matches = {}
+        dupes = set()
+        for box in unit:
+            if len(values[box]) == 1:
+                dupes.add(values[box])
+                continue
+            for number in values[box]:
+                if number in matches:
+                    dupes.add(number)
+                else:
+                    matches[number] = box
+        # anything left was the only option for that number
+        for number, box in matches.items():
+            if number in dupes:
+                continue
+            values[box] = number
+    return values
 
 
 def reduce_puzzle(values):
@@ -110,8 +134,22 @@ def reduce_puzzle(values):
         The values dictionary after continued application of the constraint strategies
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Reduce by use of Eliminate and Only Choice strategy
+        values = eliminate(values)
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -133,8 +171,33 @@ def search(values):
     You should be able to complete this function by copying your code from the classroom
     and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    # values_mod is a copy of values so the originals are unaffected
+    values_mod = dict(values)
+    values_mod = reduce_puzzle(values_mod)
+    if values_mod is False:
+        # Discovered it's not a valid solution, skip out here
+        return False
+    # Get the list of boxes with more than one possibility
+    unsolved = [box for box in values_mod if len(values_mod[box]) > 1]
+    # If there are none, this is the solution; return it.
+    if len(unsolved) == 0:
+        return values_mod
+
+    # Otherwise take a/the box with the minimum number of possibilities
+    min_pos_box = min(
+        unsolved,
+        key=lambda x: len(values_mod[x])
+    )
+    # For each possibility in that min box,
+    # recurse. Return the first non-false answer
+    for possible_value in values_mod[min_pos_box]:
+        candidate_solution = dict(values_mod)
+        candidate_solution[min_pos_box] = possible_value
+        candidate_solution = search(candidate_solution)
+        if not (candidate_solution is False):
+            return candidate_solution
+    # If none returned, all came back false, so the whole branch is bad.
+    return False
 
 
 def solve(grid):
